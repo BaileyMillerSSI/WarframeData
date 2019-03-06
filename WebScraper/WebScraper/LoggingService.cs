67c278b6a53eefe97a0095bfad455cd5575eefe6
Bailey Miller <bjm19@pct.edu>
2019-03-06 18:20:59 -0500

    Good start on scraping all the data.

---
+++ WebScraper/WebScraper/LoggingService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    public static class LoggingService
+    {
+        public static Dictionary<DateTime, string> Log
+        {
+            get
+            {
+                return conCurLog.ToDictionary(entry => entry.Key, entry => entry.Value);
+            }
+        }
+
+        private static ConcurrentDictionary<DateTime, string> conCurLog = new ConcurrentDictionary<DateTime, string>();
+
+        /// <summary>
+        /// Adds a new event to the event stream being posted to the server
+        /// </summary>
+        /// <param name="message">The message to be logged</param>
+        public static void LogEvent(string message)
+        {
+            conCurLog.TryAdd(DateTime.Now, message);
+        }
+
+
+        /// <summary>
+        /// Will setup a socket connection to the server to broadcast events inside the execution of the code
+        /// </summary>
+        public static void PrepareLoggingService()
+        {
+            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+            LogEvent("Service is ready");
+        }
+    }
+}
---
+++ WebScraper/WebScraper/Program.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    class Program
+    {
+        const string WikiLink = "https://warframe.fandom.com/wiki/Category:Primary_Weapons";
+        const string RootWikiLink = "https://warframe.fandom.com";
+
+        static HttpClient _Web = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparing to download Primary Weapon Data from Warframe Wiki");
+
+            LoggingService.PrepareLoggingService();
+
+            var data = GetWeaponListAsync().Result;
+            
+            Console.WriteLine("\n\n");
+
+            foreach (var wep in data)
+            {
+                Console.WriteLine($"{wep.Title} - '{wep.CodexEntry}'");
+                Console.WriteLine($"Credits: {wep.CreditSellPrice}");
+            }
+
+
+            Console.ReadKey();
+        }
+
+
+        static async Task<List<WeaponData>> GetWeaponListAsync()
+        {
+            var rawHtmlData = await _Web.GetStringAsync(WikiLink).ConfigureAwait(false);
+
+            var webDoc = new HtmlDocument();
+            webDoc.LoadHtml(rawHtmlData);
+
+            // With LINQ 
+            var nodes = webDoc.DocumentNode.Descendants("li")
+             .Where(x => x.HasClass("category-page__member"))
+             .Select(x => x.Descendants())
+             .Select(x => new WeaponData(x.Where(inner => inner.Name == "a" && inner.HasClass("category-page__member-link")).Select(innerVal => innerVal.Attributes["title"].Value).FirstOrDefault(), x.Where(inner => inner.Name == "a" && inner.HasClass("category-page__member-link")).Select(innerVal => string.Concat(RootWikiLink, innerVal.Attributes["href"].Value)).FirstOrDefault())
+             {
+                 ThumbnailImage = x.Where(inner=> inner.Name == "div" && inner.HasClass("category-page__member-left")).SelectMany(divDesc=> divDesc.Descendants()).Where(innerDiv=>innerDiv.Name == "img").Where(imgSrc=>imgSrc.Attributes["src"].Value.Contains("http")).Select(img=> img.Attributes["src"].Value).FirstOrDefault()
+             })
+             .Where(x=>!x.Title.Contains("Conclave") && !x.Title.Contains("File:") && !x.Title.Contains("Category:") && !x.Title.Contains(" (Weapon)"))
+             .ToList();
+
+            //await nodes.FirstOrDefault().LoadDataAsync(_Web);
+
+            nodes.AsParallel().ForAll((wep)=> 
+            {
+                try
+                {
+                    LoggingService.LogEvent($"Loading {wep.Title} into local cache");
+                    wep.LoadDataAsync(_Web).Wait();
+                }
+                catch (Exception)
+                {
+
+                }
+            });
+
+            return nodes;
+        }
+    }
+}
